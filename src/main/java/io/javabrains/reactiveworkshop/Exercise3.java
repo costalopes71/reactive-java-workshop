6d0453444bfd2139fa9e7957096723542d81869f
joao.lopes <joaolopes71@hotmail.com>
2023-12-09 01:51:20 -0300

    [joao-study] exercicio 3

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -10,11 +11,16 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        List<Integer> numbers = ReactiveSources.intNumbersFlux()
+                .toStream()
+                .toList();
 
+        System.out.println("List size: %s and toString %s".formatted(numbers.size(), numbers));
+        // essa operacao acima eh blocante!! pois preciso aguardar todos os numeros
+        // por ser blocante, nao preciso do codigo abaixo para fazer com que o resultado seja imprimido
 
-        System.out.println("Press a key to end");
-        System.in.read();
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
 
 }
